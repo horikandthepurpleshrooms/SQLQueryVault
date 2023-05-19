f2edcd57542b0ac6807b720a66ab428cd877805c
Horik <h22ea@icloud.com>
2023-05-19 17:06:44 +0300

    added selectors and comparison operators

--- Core/essential_queries.sql
+++ Core/essential_queries.sql
@@ -4,6 +4,22 @@
    multi-line comment
    in SQL */
 
+/*
+COMPARISON OPERATORS: <, >, <=, >=, =, <>, !=, !<, !>, AND, OR
+
+ = (Equals)
+ > (Greater Than)
+ < (Less Than)
+ >= (Greater Than or Equal To)
+ <= (Less Than or Equal To)
+ <> (Not Equal To)
+ != (Not Equal To, non-ISO standard)
+ !< (Not Less Than, non-ISO standard)
+ !> (Not Greater Than, non-ISO standard)
+ AND: Combines two or more conditions, requiring all conditions to be true for a row to be included in the result set.
+ OR: Combines two or more conditions, allowing any of the conditions to be true for a row to be included in the result set
+*/
+
 -- Creating a table
 CREATE TABLE student (
 	student_id INT,
@@ -17,6 +33,30 @@ DROP TABLE student;
 
 -- Returns everything from the specified location
 SELECT * FROM student;
+
+SELECT student.name, student.major
+FROM student;
+
+SELECT *
+FROM student
+ORDER BY major, student_id; -- Default is ASC, you can specify DESC if wanted
+
+SELECT *
+FROM student
+LIMIT 3;
+
+SELECT *
+FROM student
+ORDER BY student_id DESC
+LIMIT 2;
+
+SELECT *
+FROM student
+WHERE major = "CompSci";
+
+SELECT name, major
+FROM student
+WHERE major = "CompSci" OR major = "Engineering";
 
 /* Retrieve information about the structure of a table.
    It provides metadata about the columns or fields within a table. */
